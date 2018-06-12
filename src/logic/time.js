4dae4cf7fbb701c072d3222679d411427530e296
letti <aletta.geczi@ge.com>
2018-06-12 14:31:42 +0200

    dunno what i did

--- src/logic/time.js
+++
@@ -1,8 +0,0 @@
-const start = new Date().getTime();
-setInterval(()=>{
-    let now = new Date().getTime();
-    let distance = now - start;
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);  
-    document.getElementsByClassName("timer")[0].innerHTML = minutes + "m " + seconds + "s ";
-}, 1000);
