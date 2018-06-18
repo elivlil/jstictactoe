379f43dea191f6afcd4f0da5f8c26a9b11a5f41c
letti <aletta.geczi@ge.com>
2018-06-18 13:49:20 +0200

    change white colors

--- src/js/logic/Ai.js
+++ src/js/logic/Ai.js
@@ -5,38 +5,40 @@ class Ai {
   }
 
   evaluatePosition(game) {
+    //this is a recursive function
     if (game.round === -1) {
       return this.getScore(game);
-
-    }
-    if (game.currentMark() === "o") {
-      let bestValue = -Infinity;
-      let iterator = game.board.getEmptyPositions();
-      let nextEmptyPosition = iterator.next();
-      while (!nextEmptyPosition.done) {
-        let x = nextEmptyPosition.value.row;
-        let y = nextEmptyPosition.value.col;
-        let childGame = game.copy();
-        childGame.play(x, y);
-        let childResult = this.evaluatePosition(childGame);
-        bestValue = Math.max(bestValue, childResult);
-        nextEmptyPosition = iterator.next();
-      }
-      return bestValue;
+    } else if (game.currentMark() === "o") {
+      return this.evalChild(game, -Infinity, Math.max);
     } else {
-      let bestValue = Infinity;
-      let iterator = game.board.getEmptyPositions();
-      let nextEmptyPosition = iterator.next();
-      while (!nextEmptyPosition.done) {
-        let x = nextEmptyPosition.value.row;
-        let y = nextEmptyPosition.value.col;
-        let childGame = game.copy();
-        childGame.play(x, y);
-        let childResult = this.evaluatePosition(childGame);
-        bestValue = Math.min(bestValue, childResult);
-        nextEmptyPosition = iterator.next();
-      }
-      return bestValue;
+      return this.evalChild(game, Infinity, Math.min);
+    }
+  }
+
+  evalChild(game, defaultValue, selectorFunction) {
+    let bestValue = defaultValue;
+    let iterator = game.board.getEmptyPositions();
+    let nextEmptyPosition = iterator.next();
+    while (!nextEmptyPosition.done) {
+      let x = nextEmptyPosition.value.row;
+      let y = nextEmptyPosition.value.col;
+      let childGame = game.copy();
+      childGame.play(x, y);
+      //recursion occurs here
+      let childResult = this.evaluatePosition(childGame);
+      bestValue = selectorFunction(bestValue, childResult);
+      nextEmptyPosition = iterator.next();
+    }
+    return bestValue;
+  }
+
+  getScore(game) {
+    if (game.winner === "x") {
+      return -10 * game.weight;
+    } else if (game.winner === "o") {
+      return 10 * (10 - game.weight);
+    } else {
+      return 0;
     }
   }
 
@@ -49,21 +51,12 @@ class Ai {
       let y = nextEmptyPosition.value.col;
       let newGame = this.realGame.copy()
       newGame.play(x, y);
+      //calls recursive function
       let val = this.evaluatePosition(newGame);
-      moves.push({ pos: new Position(x, y), score: val });
+      moves.push({pos: new Position(x, y), score: val});
       nextEmptyPosition = iterator.next();
     }
     moves.sort((move1, move2) => move2.score - move1.score);
     return moves[0];
-  }
-
-  getScore(game) {
-    if (game.winner === "x") {
-      return -10 * game.weight;
-    } else if (game.winner === "o") {
-      return 10  * (10-game.weight);
-    } else {
-      return 0;
-    }
   }
 }
