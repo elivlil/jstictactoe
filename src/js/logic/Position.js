91798e5be7844ca011e544465dc8f069c80112f8
letti <aletta.geczi@ge.com>
2018-06-18 11:11:23 +0200

    reverted

---
+++ src/js/logic/Position.js
@@ -0,0 +1,13 @@
+/*import {Timer} from './src/js/dom/Timer.js'
+import {Position} from './src/js/logic/Position.js'
+import {Ai} from './src/js/logic/Ai.js'
+import {Board} from './src/js/logic/Board.js'
+import {Game} from './src/js/logic/Game.js'
+import {Player} from './src/js/logic/Player.js'*/
+
+export class Position {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  }
+}
